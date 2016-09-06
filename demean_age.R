0777d382e6d3fac8b9e5b6a21f2eed74314caea6
mmandelli <marialuisa.mandelli@gmail.com>
2016-09-05 18:06:58 -0700

    bla

--- demean_age.R
+++ demean_age.R
@@ -1,6 +1,6 @@
 demographics <- read.csv(file='participants.tsv', head=TRUE, sep="\t")
 age <- demographics[4]
-demean_age <- age - sum(age)/lengt(age)
+demean_age <- age - sum(age)/length(age)
 
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
